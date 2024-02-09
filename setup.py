75ed448e739b5b72bdb6a167ddfc176c696bd813
Nikita Sherstianykh <n.sherstianykh@gmail.com>
2024-02-10 00:08:11 +0100

    Update setup.py
    - Specified authorship for transparency and accountability.
    - Updated required libraries.
    - Changed the main command to 'file-analyzer.'

--- setup.py
+++ setup.py
@@ -2,16 +2,18 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name='File-System-Analyzer',
-    version='0.1.1',
-    description='Personal virtual assistant for files in system.',
+    version='0.1.4',
+    description="Personal virtual assistant for files in system.",
     url='https://github.com/Thoughtseize1/File_System_Insight_Tool',
+    author="Mykyta Sherstianykh",
+    author_email="mykyta.sherstianykh@gmail.com",
     packages=find_namespace_packages(),
     install_requires=[
-        'Faker',
+        'tabulate',
     ],
     entry_points={
         'console_scripts': [
-            'hello_my=src.main:main',
+            'file-analyzer=src.main:main',
         ],
     },
 )
