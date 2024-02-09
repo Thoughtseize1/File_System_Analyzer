4554d07177a035172f98c2f31e2a376fa9a21276
Nikita Sherstianykh <n.sherstianykh@gmail.com>
2024-02-10 00:06:55 +0100

    Update main logic
    Moved color coding logic to Config class, added KeyboardInterrupt handling

--- src/main.py
+++ src/main.py
@@ -1,26 +1,22 @@
 from src.parser import parser
-
-RESET_COLOR = "\033[0m"
-YELLOW_COLOR = "\033[33m"
-CYAN_COLOR = "\033[36m"
-RED_COLOR = "\033[31m"
+from src.manager import manager
 
 
 def main():
     """ Main function - all interaction with user """
-    print(f"{YELLOW_COLOR}HELLO MAN! How can i help you? Use can type 'help' to get available commands{RESET_COLOR}")
-    while True:
-        meow = 0
-        msg = input(f"{CYAN_COLOR}Input command{RESET_COLOR}: ")
-        command, params = parser(msg)
-        if command:
-            command(*params)
-            meow += 1
-            if meow == 4:
-                print('Meow!')
-                meow = 0
-        else:
-            print(f"{RED_COLOR}No command specified")
+    print(
+        f"{manager.colors['YELLOW']}HELLO MAN! How can i help you? Use can type 'help' to get available commands{manager.colors['RESET']}")
+    try:
+        while True:
+            msg = input(f"{manager.colors['CYAN']}Input command{manager.colors['RESET']}: ")
+            command, params = parser(msg)
+            if command:
+                command(*params)
+            else:
+                print(f"{manager.colors['RED']}No command specified")
+    except KeyboardInterrupt:
+        print("\nGoodbye!")
+        exit()
 
 
 if __name__ == '__main__':
