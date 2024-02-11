6510153be2bfc2006a76dd4042482e224f9cc28f
Nikita Sherstianykh <n.sherstianykh@gmail.com>
2024-02-11 17:30:00 +0100

    Update setup.py
    - Changing setup for upload package

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 from setuptools import setup, find_namespace_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name='File-System-Analyzer',
-    version='0.1.4',
+    version='0.1.6',
     description="Personal virtual assistant for files in system.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url='https://github.com/Thoughtseize1/File_System_Insight_Tool',
     author="Mykyta Sherstianykh",
     author_email="mykyta.sherstianykh@gmail.com",
